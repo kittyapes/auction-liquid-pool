2d38a23c9d5910308434e5c68a0177321ea20ded
kittyapes <kittyapes@gmail.com>
2023-02-24 10:12:46 +1000

    mock nft metadata

--- contracts/mock/Mock721NFT.sol
+++ contracts/mock/Mock721NFT.sol
@@ -7,9 +7,13 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 contract Mock721NFT is ERC721Enumerable {
     uint256 private lastTokenId;
 
-    constructor() ERC721("Mock NFT", "MOCK") {}
+    constructor() ERC721("Doodle", "DOODLE") {}
 
     function mint(uint256 amount) external {
         for (uint256 i; i < amount; ++i) _safeMint(msg.sender, lastTokenId++);
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return "ipfs://QmPMc4tcBsMqLRuCQtPmPe84bpSjrC3Ky7t3JWuHXYB4aS/";
     }
 }
